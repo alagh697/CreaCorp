0ac3f1565ba27e895e8c63ec494caf08a074f6a8
Alaaeddine <gharbi.alaeddine@gmail.com>
2020-02-19 10:08:31 +0100

    First Commit

---
+++ CreaCorp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CreaCorp
+//
+//  Created by Alaaeddine Gharbi on 19/02/2020.
+//  Copyright © 2020 Alaaeddine Gharbi. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
